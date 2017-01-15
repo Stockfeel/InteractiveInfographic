a486dd3c61c18b19cb2c696f2537f5b6a212db88
Joseph Wang <jamesxxxxxxx@gmail.com>
2017-01-15 16:58:33 +0800

    add fullpage

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+  // Handler for .ready() called.
+  $('#fullpage').fullpage({
+      sectionSelector: '.vertical-scrolling',
+      slideSelector: '.horizontal-scrolling',
+      // controlArrows: false
+      // more options here
+  });
+});
