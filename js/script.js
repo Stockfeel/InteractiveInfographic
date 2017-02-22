a430e707b35b94d6d7adc7d0a2f932b834df81cc
rockmandash <jamesxxxxxxx@gmail.com>
2017-02-22 20:50:42 +0800

    test force 3d if performance get better

--- js/script.js
+++ js/script.js
@@ -760,47 +760,47 @@ $(document).ready(function() {
 
 
 
-    tlScene01.from($svg01Cloud01, 0.6, { y: 100, opacity: 0 }, 0.3)
-             .from($svg01Cloud02, 0.6, { y: 100, opacity: 0 }, 0.6)
-             .from($svg01Cloud03, 0.6, { y: 100, opacity: 0 }, 0.9)
-             .from($svg01Cloud04, 0.6, { y: 100, opacity: 0 }, 1.1)
-             .from($svg01Cloud05, 0.6, { y: 100, opacity: 0 }, 1.3)
-             .fromTo($svg01Plane, 4, { x: -1500, y: 500, scale: 2 }, { x: 1500, y: -500, scale: 0.2 }, 0.9)
+    tlScene01.from($svg01Cloud01, 0.6, { y: 100, opacity: 0, force3D:true }, 0.3)
+             .from($svg01Cloud02, 0.6, { y: 100, opacity: 0, force3D:true }, 0.6)
+             .from($svg01Cloud03, 0.6, { y: 100, opacity: 0, force3D:true }, 0.9)
+             .from($svg01Cloud04, 0.6, { y: 100, opacity: 0, force3D:true }, 1.1)
+             .from($svg01Cloud05, 0.6, { y: 100, opacity: 0, force3D:true }, 1.3)
+             .fromTo($svg01Plane, 4, { x: -1500, y: 500, scale: 2, force3D:true }, { x: 1500, y: -500, scale: 0.2, force3D:true }, 0.9)
              .to([$svg01Cloud01,
                   $svg01Cloud02,
                   $svg01Cloud03,
                   $svg01Cloud04,
                   $svg01Cloud05
-                  ], 0.8, { y: -1500, opacity: 0, ease: Back.easeIn.config(1.7) }, 2.8)
+                  ], 0.8, { y: -1500, opacity: 0, ease: Back.easeIn.config(1.7), force3D:true }, 2.8)
 
 
-             .from($svg01Character01, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 3.6)
-             .from($svg01Character02, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 3.8)
-             .from($svg01Character03, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 4)
-             .from($svg01Character04, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 4.2)
-             .from($svg01Character05, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 4.4)
-             .from($svg01Character06, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 4.6)
-             .from($svg01Character07, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut }, 4.8)
-             .to($svg01WholeCharacter, 0.8, { y: -240, scale: 0.8, transformOrigin: '50% 50%', ease: Power1.easeOut }, 5.4)
+             .from($svg01Character01, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 3.6)
+             .from($svg01Character02, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 3.8)
+             .from($svg01Character03, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 4)
+             .from($svg01Character04, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 4.2)
+             .from($svg01Character05, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 4.4)
+             .from($svg01Character06, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 4.6)
+             .from($svg01Character07, 0.7, { y: -100, opacity: 0, scale: 3, ease: Power1.easeOut, force3D:true }, 4.8)
+             .to($svg01WholeCharacter, 0.8, { y: -240, scale: 0.8, transformOrigin: '50% 50%', ease: Power1.easeOut, force3D:true }, 5.4)
 
 
-             .from($svg01ChinaVsJapan, 1, { y: -100, opacity: 0 }, 6.1)
+             .from($svg01ChinaVsJapan, 1, { y: -100, opacity: 0, force3D:true }, 6.1)
 
-             .from($svg01Door, 0.4, { y: -100, opacity: 0 }, 6.2)
+             .from($svg01Door, 0.4, { y: -100, opacity: 0, force3D:true }, 6.2)
 
-             .from($svg01Bg, 0.4, { y: -100, opacity: 0 }, 6.3)
+             .from($svg01Bg, 0.4, { y: -100, opacity: 0, force3D:true }, 6.3)
 
-             .from($svg01People01, 0.4, { x: -100, opacity: 0 }, 6.4)
-             .from($svg01People02, 0.4, { x: -100, opacity: 0 }, 6.5)
-             .from($svg01People03, 0.4, { x: -100, opacity: 0 }, 6.6)
-             .from($svg01People04, 0.4, { x: -100, opacity: 0 }, 6.7)
-             .from($svg01People05, 0.4, { x: 100, opacity: 0 }, 6.8)
-             .from($svg01People06, 0.4, { x: 100, opacity: 0 }, 6.9)
-             .from($svg01People07, 0.4, { x: 100, opacity: 0 }, 7)
+             .from($svg01People01, 0.4, { x: -100, opacity: 0, force3D:true }, 6.4)
+             .from($svg01People02, 0.4, { x: -100, opacity: 0, force3D:true }, 6.5)
+             .from($svg01People03, 0.4, { x: -100, opacity: 0, force3D:true }, 6.6)
+             .from($svg01People04, 0.4, { x: -100, opacity: 0, force3D:true }, 6.7)
+             .from($svg01People05, 0.4, { x: 100, opacity: 0, force3D:true }, 6.8)
+             .from($svg01People06, 0.4, { x: 100, opacity: 0, force3D:true }, 6.9)
+             .from($svg01People07, 0.4, { x: 100, opacity: 0, force3D:true }, 7)
 
              .from($svg01BottomText, 0.4, BottomTextParameter, 7.1)
 
-             .from($svg01ScrollDown, 0.4, { y: 10, opacity: 0 }, 7.4);
+             .from($svg01ScrollDown, 0.4, { y: 10, opacity: 0, force3D:true }, 7.4);
 
 
 
